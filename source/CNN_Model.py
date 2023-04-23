536681247e897810e3bafec69bd27a8c9fd6cb1f
Jose Torronteras <7280807+josetorronteras@users.noreply.github.com>
2023-04-24 00:43:27 +0200

    Update Notebook

--- source/CNN_Model.py
+++ source/CNN_Model.py
@@ -12,9 +12,8 @@ class CNNModel(object):
             X: Data
         """
         self.NB_FILTERS = int(config['CNN_CONFIGURATION']['NB_FILTERS']) 
-        self.NB2_FILTERS = int(config['CNN_CONFIGURATION']['NB2_FILTERS'])
-        self.CONV1_SIZE = int(config['CNN_CONFIGURATION']['CONV1_SIZE'])
-        self.CONV2_SIZE = int(config['CNN_CONFIGURATION']['CONV2_SIZE'])
+        self.CONV_SIZE = int(config['CNN_CONFIGURATION']['CONV_SIZE'])
+        self.POOL_SIZE = int(config['CNN_CONFIGURATION']['POOL_SIZE'])
         self.NUM_CLASSES = int(config['CNN_CONFIGURATION']['NUM_CLASSES'])
         self.INPUT_SHAPE = X.shape
         
@@ -25,21 +24,17 @@ class CNNModel(object):
             model: CNN model
         """
         model = Sequential()
-        model.add(Conv2D(32, (self.CONV1_SIZE , self.CONV1_SIZE ), activation='relu', input_shape=self.INPUT_SHAPE))
-        model.add(Conv2D(32, (self.CONV1_SIZE, self.CONV1_SIZE), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(self.CONV2_SIZE , self.CONV2_SIZE )))
-        model.add(Dropout(0.25))
-        
-        model.add(Conv2D(64, (self.CONV1_SIZE, self.CONV1_SIZE), activation='relu'))
-        model.add(Conv2D(64, (self.CONV1_SIZE, self.CONV1_SIZE), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(self.CONV2_SIZE , self.CONV2_SIZE )))
-        model.add(Dropout(0.25))
-        
+
+        model.add(Conv2D(self.NB_FILTERS, (self.CONV_SIZE, self.CONV_SIZE ), input_shape=(150, 150, 3), activation='relu', strides=2))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Conv2D(self.NB_FILTERS, (self.CONV_SIZE, self.CONV_SIZE ), strides=2, activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
         model.add(Flatten())
-        model.add(Dense(512, activation='relu'))
-        model.add(Dropout(0.5))
+        model.add(Dense(150, activation='relu'))
         model.add(Dense(self.NUM_CLASSES, activation='softmax'))
-        
+
         return model
         
         

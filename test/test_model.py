2dcdfea096dd8d37fca4068e0a400ed74c944bbb
Jose Torronteras <jose.torronteras@bcncgroup.com>
2023-10-19 19:29:35 +0200

    Add Testing action

---
+++ test/test_model.py
@@ -0,0 +1,14 @@
+from cnn_model import CNNModel
+import configparser
+
+config = configparser.ConfigParser()
+config.read('testing-config.ini')
+
+def test_model_creation():
+    model = CNNModel(config, train_generator[0][0][0]).build_model()
+    assert model is not None
+
+def test_model_compilation():
+    model = CNNModel(config, train_generator[0][0][0]).build_model()
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+    assert len(model.layers) > 0

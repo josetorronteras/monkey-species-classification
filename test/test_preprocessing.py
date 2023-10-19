989c50d286cdff88c09a131880d1a25def693875
Jose Torronteras <jose.torronteras@bcncgroup.com>
2023-10-19 23:59:21 +0200

    test_preprocessing_images test

---
+++ test/test_preprocessing.py
@@ -0,0 +1,21 @@
+from source.preprocessing_images import PreprocessingImages
+
+@pytest.mark.parametrize("config", [
+    (
+        {
+            'PATH_CONFIGURATION': {
+                'TRAIN_IMAGES_PATH': 'tests/images/training',
+                'VALIDATION_IMAGES_PATH': 'tests/images/validation'
+            },
+            'IMAGE_FEATURES': {
+                'SIZE': '150'
+            }
+        }
+    ),
+])
+def test_preprocessing_images(config):
+    preprocessing = PreprocessingImages(config)
+    train_generator, validation_generator = preprocessing.preprocessImages()
+
+    assert train_generator is not None
+    assert validation_generator is not None

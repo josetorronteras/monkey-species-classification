4788de935bc5d678487a2270ff5cb4a5ef54c3fa
Jose Torronteras <jose.torronteras@bcncgroup.com>
2023-10-19 22:42:43 +0200

    update test

--- test/test_model.py
+++ test/test_model.py
@@ -1,12 +1,28 @@
-from source.cnn_model import CNNModel, train_generator
-import configparser
-
-config = configparser.ConfigParser()
-config.read('testing-config.ini')
+import pytest
+from source.cnn_model import CNNModel
 
 def test_model_creation():
-    model = CNNModel(config, train_generator[0][0][0]).build_model()
-    assert model is not None
+    config = {
+        'CNN_CONFIGURATION': {
+            'NB_FILTERS': 32,
+            'CONV_SIZE': 3,
+            'POOL_SIZE': 2,
+            'NUM_CLASSES': 10
+        }
+    }
+    
+    mock_image_data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    mock_batch_data = [mock_image_data]
+    mock_generator_data = [[mock_batch_data]]
+
+    cnn_model = CNNModel(config, mock_generator_data)
+
+    try:
+        model = cnn_model.build_model()
+        assert model is not None
+    except Exception as e:
+        pytest.fail(f'Error build model: {e}')
+
 
 def test_model_compilation():
     model = CNNModel(config, train_generator[0][0][0]).build_model()
